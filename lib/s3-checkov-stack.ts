6984029387e94f17466e4ddaf32a35b6e60bcaeb
Your Name <srinathbala00@gmail.com>
2023-07-26 16:24:50 +0530

    first commit

--- lib/s3-checkov-stack.ts
+++ lib/s3-checkov-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class S3CheckovStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3CheckovQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const Bucket = new s3.Bucket(this, `S3-checkov-15-07-23`, {
+      publicReadAccess: true,
+      bucketName: `s3-checkov-15-07-23`,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false
+      })
+    });
   }
 }
