cc1d66e77eb7dd06fa6a6c6669a9977552dca59a
Your Name <srinathbala00@gmail.com>
2023-08-09 12:06:09 +0530

    first commit

--- lib/s3-checkov-stack.ts
+++ lib/s3-checkov-stack.ts
@@ -6,7 +6,7 @@ export class S3CheckovStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const Bucket = new s3.Bucket(this, `S3-checkov-15-07-23`, {
+    const bucket = new s3.Bucket(this, `S3-checkov-15-07-23`, {
       publicReadAccess: true,
       bucketName: `s3-checkov-15-07-23`,
       encryption: s3.BucketEncryption.S3_MANAGED,
@@ -17,5 +17,18 @@ export class S3CheckovStack extends cdk.Stack {
         restrictPublicBuckets: false
       })
     });
+
+    const cfnBucket = bucket.node.defaultChild as s3.CfnBucket;
+
+    cfnBucket.cfnOptions.metadata = {
+      'checkov': {
+        'skip': [
+          {
+            'id': 'CKV_AWS_18',
+            'comment': 'No need to ensure the S3 bucket has access logging enabled'
+          }
+        ]
+      }
+    }
   }
 }
